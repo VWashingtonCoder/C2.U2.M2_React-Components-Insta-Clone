9984f6b493961dc63ba33b52fb30d9cb4d56ef6c
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-15 16:36:07 -0700

    updated App.js likePost() & LikeSection done

--- src/App.js
+++ src/App.js
@@ -32,9 +32,10 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    setPosts(posts.map(id => {
-      return postId === id ? {...posts} : posts;
-    }))
+    const updatedLikeNum = posts.map(post => {
+      return post.id === postId ? {...post, likes: post.likes + 1} : post;
+    })
+    setPosts(updatedLikeNum)
   };
 
   return (
