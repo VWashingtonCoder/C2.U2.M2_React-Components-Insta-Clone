01c2c3e6bb577fabe7aa639acc58fc6c7ba1e17d
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-15 15:42:34 -0700

    App.js & Posts.js done

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Post from './Post';
 import './Posts.css';
+import Post from './Post'
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
@@ -9,7 +9,10 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
+      {/* Check the implementation of Post to see what props it requires!_post & likePost */}
+      {posts.map(post => {
+        return <Post post={post} likePost={likePost} key={post.id} />
+      })}
     </div>
   );
 };
